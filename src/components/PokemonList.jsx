d4008c7cc8c612d2e316f3ed7a8949f7ce84cbef
Suyog Satyal <62869132+suyogsatyal@users.noreply.github.com>
2023-05-25 12:51:42 +0545

    UI Changes

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -11,9 +11,7 @@ function PokemonList() {
             .then(data => {
                 setPokemonList(data.results);
                 if (data.next == null) {
-                    setNext(
-                        next = false
-                    )
+                    setNext(false)
                 }
             })
             .catch(error => {
@@ -22,14 +20,21 @@ function PokemonList() {
     }, [offset])
 
     return (
-        <div>
-            <h1>Pokemon List</h1>
+        <div className="py-4">
+            <h1 className="pokedex text-center text-5xl md:text-7xl text-gray-900">PokéDex</h1>
+            <h3 className="pokedex text-center text-lg md:text-xl text-gray-600">Made using <a href="pokeapi.co" className=" text-blue-400">PokeApi</a></h3>
+            <div className="flex flex-row justify-between py-2">
+                <button onClick={() => setOffset(offset - 24)} className={offset === 0 ? 'hidden' : 'block' + 'inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none'}>Previous</button>
+                <button></button>
+                <button onClick={() => setOffset(offset + 24)} className="inline-flex items-center justify-center right px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none">Next</button>
+            </div>
+
             <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3">
                 {pokemonList.map((pokemon, index) => (
-                    <a href="#" key={pokemon.name} className="flex flex-col items-center px-3 bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-normal to-fire rounded-md">
+                    <a href="#" key={pokemon.name} className="flex flex-col items-center px-1 sm:px-3 overflow-hidden bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-normal to-fire rounded-md">
                         <p className="name capitalize text-gray-50 text-xl">{pokemon.name}</p>
                         <div className="flex flex-row items-center">
-                            <img class="md:w-32 md:h-32 w-24 h-24 z-10" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + offset + 1}.svg`} />
+                            <img className="md:w-32 md:h-32 w-24 h-24 z-10" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + offset + 1}.svg`} />
                             <span className="index md:-ml-24 -ml-16 z-0 font-bold tracking-wider text-white opacity-50 md:text-7xl text-6xl">#{(index + offset + 1).toString().padStart(3, '0')}</span>
                         </div>
 
@@ -37,11 +42,12 @@ function PokemonList() {
                 ))}
             </ul>
 
-            <button onClick={() => setOffset(offset - 24)} className={offset === 0 ? 'hidden' : 'block'}>
-                Previous
-            </button>
 
-            <button onClick={() => setOffset(offset + 24)} className={next === true ? 'block' : 'hidden'}>Next</button>
+            <div className="flex flex-row justify-between py-2">
+                <button onClick={() => setOffset(offset - 24)} className={offset === 0 ? 'hidden' : 'block' + 'inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none'}>Previous</button>
+                <button></button>
+                <button onClick={() => setOffset(offset + 24)} className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none">Next</button>
+            </div>
         </div>
     );
 }
