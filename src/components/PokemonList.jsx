2745d9b3f5797204b6a556857a03101bf7ee664c
Suyog Satyal <62869132+suyogsatyal@users.noreply.github.com>
2023-05-25 16:21:24 +0545

    Adjusted Pokemon ID and Image Spacing

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -22,20 +22,20 @@ function PokemonList() {
     return (
         <div className="py-4">
             <h1 className="pokedex text-center text-5xl md:text-7xl text-gray-900">PokéDex</h1>
-            <h3 className="pokedex text-center text-lg md:text-xl text-gray-600">Made using <a href="pokeapi.co" className=" text-blue-400">PokeApi</a></h3>
+            <h3 className="pokedex text-center text-lg md:text-xl text-gray-600">Made using <a href="https://pokeapi.co" className=" text-blue-400">PokeApi</a></h3>
             <div className="flex flex-row justify-between py-2">
                 <button onClick={() => setOffset(offset - 24)} className={offset === 0 ? 'hidden' : 'block' + 'inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none'}>Previous</button>
                 <button></button>
                 <button onClick={() => setOffset(offset + 24)} className="inline-flex items-center justify-center right px-4 py-2 text-base font-medium leading-6 text-gray-600 whitespace-no-wrap bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none">Next</button>
             </div>
 
-            <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3">
+            <ul className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-3">
                 {pokemonList.map((pokemon, index) => (
                     <a href="#" key={pokemon.name} className="flex flex-col items-center px-1 sm:px-3 overflow-hidden bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-normal to-fire rounded-md">
                         <p className="name capitalize text-gray-50 text-xl">{pokemon.name}</p>
-                        <div className="flex flex-row items-center">
-                            <img className="md:w-32 md:h-32 w-24 h-24 z-10" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + offset + 1}.svg`} />
-                            <span className="index md:-ml-24 -ml-16 z-0 font-bold tracking-wider text-white opacity-50 md:text-7xl text-6xl">#{(index + offset + 1).toString().padStart(3, '0')}</span>
+                        <div className="flex flex-row items-center justify-between w-full py-1 md:py-3">
+                            <img className="xl:w-32 xl:h-32 md:w-28 md:h-28 sm:w-24 sm:h-24 w-16 h-16 z-10" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + offset + 1}.svg`} />
+                            <span className="index -ml-12 lg:-ml-12 xl:-ml-9 z-0 font-bold tracking-wider text-white opacity-50  text-4xl md:text-6xl sm:text-5xl">#{(index + offset + 1).toString().padStart(3, '0')}</span>
                         </div>
 
                     </a>
